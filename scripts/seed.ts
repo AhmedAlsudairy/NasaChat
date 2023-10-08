4b5cc5193958ea2cc54c2a96e8647c2b5a618d6b
Ahmed <gfg59204@gmail.com>
2023-10-08 07:19:49 +0400

    for deloyment

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,22 +1,14 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 
 const db = new PrismaClient();
 
 async function main() {
   try {
     await db.category.createMany({
-      data: [
-        { name: 'Space Explorers' },
-        { name: 'Space Movies & TV' },
-        { name: 'Space Musicians' },
-        { name: 'Space Games' },
-        { name: 'Space Animals' },
-        { name: 'Astronomy' },
-        { name: 'Space Scientists' },
-      ],
+      data: [{ name: "Astronomy" }, { name: "Space Scientists" }],
     });
   } catch (error) {
-    console.error('Error seeding space-related categories:', error);
+    console.error("Error seeding space-related categories:", error);
   } finally {
     await db.$disconnect();
   }
